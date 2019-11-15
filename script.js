97d86f83a07845bd193878e464d9076bf8c5694f
Sonja Parsell <hello@sparsell.com>
2019-11-15 11:00:20 -0500

    updates to slider/imgs

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Great job - you're here not spending money!");
+
+function getDate() {
+var d = new Date();
+var currentMonth = d.getMonth;
+var dayOfMonth = d.getDate;
+return getDate;
+}
+
+getDate();
+
